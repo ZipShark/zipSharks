26346bc6e39001f123e32d6bb3e5b3bcd9756531
mustafa-boorenie <75136994+mustafa-boorenie@users.noreply.github.com>
2022-07-11 16:09:51 +0100

    volume attribute -> audio player

--- client/src/components/HomeSection/HomeSection.js
+++ client/src/components/HomeSection/HomeSection.js
@@ -38,6 +38,7 @@ function HomeSection() {
             autoPlay={true}
             controls={false}
             loop={true}
+            volume={0.2}
             />
     </HomeContainer>
     </>
